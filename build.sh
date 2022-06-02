f177acd5e2805f4cbc986aeaaeae52c4d75c4dac
Cloyd Van <abcd123.letters@gmail.com>
2022-06-02 18:24:21 +0800

    Modify build script

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
-echo -e "**** Redirecting to src ****"
-cd src/
 echo -e "**** Recompiling source files ****"
 sh ./compile.sh
-echo -e "**** Redirecting to / ****"
-cd ../
 
 echo -e "\n \n"
 echo -e "██████████████████████████████████████████████████████████████████"
@@ -11,8 +7,6 @@ echo -e "Attempting to build an executable file.    ::      Format ==> .jar"
 echo -e "██████████████████████████████████████████████████████████████████"
 mkdir "release"
 echo -e " \t ==> Folder created!"
-jar -cvfe release/SimpleGuessingGame.jar src/Main bin/Main.class bin/*.class src bin 
+jar -cvfe release/SimpleGuessingGame.jar bin/Main.class bin/*.class 
 echo -e "\n \n A copy of generated executable file is in ==>   release folder!"
-echo -e "---------------------------------------------------------------------"
-echo -e "\n \n Generated generated executable file is in ==>   / folder!"
 echo -e "---------------------------------------------------------------------"
